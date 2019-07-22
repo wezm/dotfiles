01c2ccd88f6a01e4a47ce5e77df330ddb6203660
Wesley Moore <wes@wezm.net>
2019-07-22 12:12:01 +1000

    Use 1px window borders

--- config/awesome/theme.lua
+++ config/awesome/theme.lua
@@ -26,7 +26,7 @@ theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
 theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(2)
+theme.border_width  = dpi(1)
 theme.border_normal = "#000000"
 theme.border_focus  = "#535d6c"
 theme.border_marked = "#91231c"
