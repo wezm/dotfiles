6fbadd9cd088cf64d8ddfebbc19ee47890734580
Wesley Moore <wes@wezm.net>
2021-07-08 10:01:54 +1000

    zsh: Remove mdv function, mdcat does this now

--- zsh/shell.zsh
+++ zsh/shell.zsh
@@ -1,9 +1,5 @@
-function fix_colours() {
+function fix-colours() {
   # Base16 Shell
   BASE16_SHELL="$HOME/.config/base16-shell/base16-default.dark.sh"
   [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
-}
-
-function mdv() {
-  (echo '<html>'; pulldown-cmark < "$1" ; echo '</html>') | elinks
 }
