f79445f5c61c7a66edb6eb534ab8573f1881e808
Wesley Moore <wes@wezm.net>
2024-07-12 09:16:47 +1000

    zsh: Add cbuild-upkg

--- zsh/chimera.zsh
+++ zsh/chimera.zsh
@@ -5,3 +5,26 @@ function cbuild-update-check() {
       done
     )
 }
+
+function cbuild-upkg() {
+  ( set -eu
+    version="$1"
+    package="$2"
+    template="$package/template.py"
+
+    if [[ ! -e ./cbuild ]]; then
+      echo "$0 must be run from cports"
+      return 1
+    fi
+
+    if [[ -z "$template" ]]; then
+      echo "Usage $0 version repo/package"
+      return 1
+    fi
+
+    #pkgver = "1.35.2"
+    sed -E "s/^pkgver = \"[^\"]+\"/pkgver = \"$version\"/" < "$template" > "${template}.tmp"
+    mv "${template}.tmp" "$template"
+    ./cbuild prepare-upgrade "$package"
+  )
+}
