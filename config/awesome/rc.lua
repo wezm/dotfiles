b3c9940cea3f4643ba1f646bf8e73fdf753e7d24
Wesley Moore <wes@wezm.net>
2025-07-25 18:01:38 +1000

    awesome: Start workspace 9 floating at start-up

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -199,7 +199,8 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8" }, s, awful.layout.layouts[1])
+    awful.tag.add("9", { layout = awful.layout.suit.floating, screen = s})
     -- awful.tag.add("First tag", {
     --     icon               = "/path/to/icon1.png",
     --     layout             = awful.layout.suit.tile,
